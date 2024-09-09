3fe92cd9dee1e3acec6d8272ead7c689ea4630a0
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2024-09-09 18:45:03 -0300

    update setup

--- 05-software-distribuition/setup.py
+++ 05-software-distribuition/setup.py
@@ -5,4 +5,12 @@ setup(
     version='0.1',
     packages=find_packages(),
     scripts=['scripts/hello.py'],
+    author='Antonio E. M. A.',
+    author_email='antonioaem@icloud.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+    ],
+    requires=['requests']
 )
